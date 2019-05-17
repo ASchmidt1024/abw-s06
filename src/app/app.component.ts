8bddc895e1e5cb74ac3e66edc5fa74d18977eb46
Alexander Schmidt <alexander.schmidt@edvas.de>
2019-05-17 15:37:56 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { MovieService } from './movie.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'A great film title';
+  imageSrc = 'https://image.tmdb.org/t/p/original/';
+  movies: [];
+
+  constructor(
+    private movieService: MovieService
+  ){
+    this.movieService.getMovies()
+    .subscribe(data => {
+      const query: any = data;
+      this.movies = query.results;
+    });
+  }
+
+  onClick(title: string) {
+    alert('You clicked movie ' + title);
+  }
+
+  clickIt(event?: MouseEvent) {
+    const message = event ? 'Propagation stopped!' : 'With propagation :-|';
+    alert('Click!' + message);
+    if (event) { event.stopPropagation(); }
+  }
+}
